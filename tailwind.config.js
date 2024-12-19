25026d1c99f10183a77445830089ff51cce21e03
Md Tasin Tahmid <tasin.tahmid048@gmail.com>
2024-12-19 18:31:47 +0600

    product page done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // content: ["./layout/*.liquid", "./templates/*.liquid"],
-  content: ["./layout/*.liquid", "./sections/*.liquid", "./snippets/*.liquid"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	// content: ["./layout/*.liquid", "./templates/*.liquid"],
+	content: [
+		"./layout/*.liquid",
+		"./sections/*.liquid",
+		"./snippets/*.liquid",
+	],
+	theme: {
+		extend: {
+			height: {
+				94: "22rem",
+			},
+		},
+	},
+	plugins: [],
 };
